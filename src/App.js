c9c0c4f950ea66ba6f032b3b4ec0997ec5f7cd83
annette Bwemere <bwemereannette@gmail.com>
2022-09-01 08:43:08 +0200

    feat: changement du titre via bind

--- src/App.js
+++ src/App.js
@@ -6,14 +6,46 @@ class App extends Component {
   state = {
     titre: "Mon Catalogue voitures",
   };
+
+  changeTitle = (e) => {
+    // console.log(e.target);
+
+    this.setState({
+      titre: "Mon nouveau titre",
+    });
+  };
+
+  changeViaParam = (titre) => {
+    this.setState({
+      titre: titre,
+    });
+  };
+
+  changeViaBind = (param) => {
+    this.setState({
+      titre: param
+    })
+  }
+
+  changeViaInput = (e) => {
+    console.log(e.target);
+  };
+
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <p>Annette</p>
-        </header> */}
-
         <Mycars title={this.state.titre} />
+
+        <button onClick={this.changeTitle}>Changer le nom en dure</button>
+        <button onClick={() => this.changeViaParam("Titre via un parametre")}>
+          Annette est dur
+        </button>
+        <button onClick={this.changeViaBind.bind(this, "Titre via un bind")}> via bind</button>
+        <input
+          type="text"
+          onChange={this.changeViaInput}
+          value={this.state.titre}
+        />
       </div>
     );
   }
--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 import Car from "./Cars";
 
 class Mycars extends Component {
-  noCopy = () => {
-    alert("Annette ne copie pas");
-  };
+  //   noCopy = () => {
+  //     alert("Annette ne copie pas");
+  //   };
+
+  //   addStyle = (e) => {
+  //     console.log(e.target);
+
+  //     if (e.target.classList.contains("styled")) {
+  //       e.target.classList.remove("styled");
+  //     } else {
+  //       e.target.classList.add("styled");
+  //     }
+  //   };
   render() {
-    console.log(this);
+    // console.log(this);
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>{this.props.title} </h1>
         <p onCopy={this.noCopy}>
           Quand la vie ne vaut plus la peine d'être vecue Quand la vie ne vaut
           plus la peine d'être vecue
